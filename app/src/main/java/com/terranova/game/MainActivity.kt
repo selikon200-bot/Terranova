cadd53accee9201c2df173ef0b5268cea3ab7e18
Eslam Hassan <selikon200@gmail.com>
2026-08-15 12:48:53 +0300

    Implement MainActivity with WebView functionality

---
+++ app/src/main/java/com/terranova/game/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.terranova.game
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    private lateinit var web: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        web = WebView(this)
+        web.settings.javaScriptEnabled = true
+        web.settings.domStorageEnabled = true
+        web.settings.allowFileAccess = true
+        web.webViewClient = WebViewClient()
+
+        web.loadUrl("file:///android_asset/index.html")
+        setContentView(web)
+    }
+
+    override fun onBackPressed() {
+        if (web.canGoBack()) {
+            web.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
